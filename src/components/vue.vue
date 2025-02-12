cd94c5fb3cf2635576dc747db4338c0c1ebbef23
BadTom <ajrih.k.i.23@gmail.com>
2025-02-12 21:51:41 +0300

    глава 1

---
+++ src/components/vue.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+		name: 'john',
+		surn: 'smit',
+        text: 'page',
+		href: 'page.html',
+        num: 5,
+        num1: 1,
+		num2: 2,
+		num3: 3,
+	}
+}
+    }
+</script>
+<template>
+  <a href="page.html">page</a>
+  <div>{{name}}</div>
+  <div>{{surn}}</div>
+  {{ num1 + num2+ num3 }}
+</template>
