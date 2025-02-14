b93c8fac2e05dd566870655337e5ac139d7ade1a
BadTom <ajrih.k.i.23@gmail.com>
2025-02-14 19:44:03 +0300

    глава 3

--- src/components/vue.vue
+++ src/components/vue.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <a href="page.html">page</a>
+    <a :href="href">page</a>
     <a href="https://www.google.com" @click.prevent>a</a>
     <div>{{ name }}</div>
     <div>{{ surn }}</div>
     <p>{{ full }}</p>
-    <p>{{ pr }}</p>
+    <p>{{ calculatedPr }}</p>
     <div>{{ num1 + num2 + num3 }}</div>
     <div>{{ arr[0] + arr[1] + arr[2] }}</div>
     <div>{{ obj.x + obj.y + obj.z }}</div>
     <input type="number" v-model.number="inputNumber">
     <button @click="showSquare">Показать квадрат</button>
-    {{ text }}
+    <div>{{ squareResult }}</div>
+    <div>{{ text }}</div>
     <button @click="changeTextToYyy">Change text to yyy</button>
     <button @click="changeTextToVvv">Change text to vvv</button>
     <button @click="changeCost">Change cost</button>
     <div>Cost: {{ cost }}</div>
-    <!-- Упрощенное управление видимостью абзацев -->
     <button @click="toggleVisible">
       Toggle Visibility {{ visible ? 'hide' : 'show' }}
     </button>
@@ -35,24 +35,81 @@
     <p v-else>---</p>
 
     <p v-if="day === 1">пон</p>
-	  <p v-if="day === 2">втор</p>
-	  <p v-if="day === 3">сре</p>
-	  <p v-if="day === 4">чет</p>
-	  <p v-if="day === 5">пят</p>
-	  <p v-if="day === 6">суб</p>
-	  <p v-if="day === 7">вос</p>
+    <p v-if="day === 2">втор</p>
+    <p v-if="day === 3">сре</p>
+    <p v-if="day === 4">чет</p>
+    <p v-if="day === 5">пят</p>
+
+    <p v-if="day === 6">суб</p>
+    <p v-if="day === 7">вос</p>
 
     <p v-if="age <= 18">подросток</p>
     <p v-else-if="age <= 25">молодой</p>
     <p v-else>дед</p>
     <button @click="togglea">
-       {{ isAuth ? 'hide' : 'show' }}
+      {{ isAuth ? 'hide' : 'show' }}
     </button>
     <div v-if="isAuth">
-		<p>+++</p>
-		<p>+++</p>
-		<p>+++</p>
-	</div>
+      <p>+++</p>
+      <p>+++</p>
+      <p>+++</p>
+    </div>
+    <p v-for="(elem, index) in items" >{{ elem }}</p>
+    <p v-for="(elem, index) in arr1" >{{ elem }}</p>
+    <p v-for="(elem, index) in items" >{{ elem**2 }}</p>
+    <ul>
+      <li v-for="(elem, index) in items" >{{ elem }}</li>
+    </ul>
+    <p v-for="(value, key, index) in obj1" >
+      {{ key }} - {{ value }} - {{ index + 1 }}
+    </p>
+    <ul>
+      <li v-for="(item, index) in items" >{{ item }}</li>
+    </ul>
+    <div v-for="(item, index) in items" >
+      <p>{{ item }}</p>
+      <p class="divider"></p>
+    </div>
+    <ul>
+      <li v-for="(item, index) in items" >
+        {{ item }}
+      </li>
+      <li class="divider"></li>
+    </ul>
+    <ul>
+      <li v-for="(link, index) in hrefs" >
+        <a :href="link.href">{{ link.text }}</a>
+      </li>
+    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in products" >
+          <td>{{ item.name }}</td>
+          <td>{{ item.price }}</td>
+          <td>{{ item.quantity }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-for="num in 30" :key="num">
+      {{ num }}
+    </p>
+      <p v-for="(elem, index) in arr2"  v-if="elem >= 0">
+        {{ elem }}
+      </p>
+       <p v-for="product in products1" :key="products1.id">
+      {{ products1.name }}
+	</p>
+  <p v-for="elem in arr1">
+		{{ elem }}
+	</p>
+	<button @click="add">add</button>
   </div>
 </template>
 
@@ -73,6 +130,7 @@ export default {
       cost: 150,
       pr: 12,
       inputNumber: 0,
+      squareResult:0,
       visible: false,
       v: true,
       e: true,
@@ -80,6 +138,38 @@ export default {
       day: 7,
       age: 25,
       isAuth: true,
+      items: [1, 2, 3, 4, 5],
+      arr1: ['a', 'b', 'c'],
+      obj1: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$',
+      },
+      hrefs: [
+        { href: '1.html', text: 'text1' },
+        { href: '2.html', text: 'text2' },
+        { href: '3.html', text: 'text3' },
+      ],
+      products: [
+        { name: 'product1', price: 100, quantity: 5 },
+        { name: 'product2', price: 200, quantity: 4 },
+        { name: 'product3', price: 300, quantity: 3 },
+      ],
+      arr2: [1, -2, 3, -4, 5],
+      products1: [
+			{
+				id: 1,
+				name: 'product1',
+			},
+			{
+				id: 2,
+				name: 'product2',
+			},
+			{
+				id: 3,
+				name: 'product3',
+			},
+		]
     };
   },
   methods: {
@@ -93,8 +183,7 @@ export default {
       this.cost = 12;
     },
     showSquare() {
-      const s = this.inputNumber * this.inputNumber;
-      alert(`Квадрат числа ${this.inputNumber} равен ${s}`);
+      this.squareResult = this.inputNumber * this.inputNumber;
     },
     toggleVisible() {
       this.visible = !this.visible;
@@ -105,15 +194,18 @@ export default {
     toggleE() {
       this.e = !this.e;
     },
-    togglea(){
-      this.isAuth = !this.isAuth
+    togglea() {
+      this.isAuth = !this.isAuth;
     },
   },
+  add: function() {
+		this.arr.push('xxx');
+	},
   computed: {
     full() {
       return this.name + ' ' + this.surn;
     },
-    pr() {
+    calculatedPr() {
       return this.cost * this.pr;
     },
   },
